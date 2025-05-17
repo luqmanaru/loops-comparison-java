8604038b268dd65ac2f60de743fe831560b2dad1
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-17 12:53:24 +0700

    Create CetakSeratusKaliDoWhile.java

---
+++ CetakSeratusKaliDoWhile.java
@@ -0,0 +1,16 @@
+package com.mycompany.cetakseratuskalidowhile;
+
+/**
+ * Demonstrasi Do-While Loop 
+ * @author luqmanaru
+ */
+public class CetakSeratusKaliDoWhile {
+    public static void main(String[] args) {
+        int count = 0;
+        
+        do {
+            System.out.println("Nama saya");
+            count++;
+        } while (count < 100);
+    }
+}
