efc541cdda066de808b797dcecff12b2ba499f6d
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-17 12:53:40 +0700

    Create CetakSeratusKaliFor.java

---
+++ CetakSeratusKaliFor.java
@@ -0,0 +1,11 @@
+/**
+ * Demonstrasi For Loop
+ * @author luqmanaru
+ */
+public class CetakSeratusKaliFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Nama saya");
+        }
+    }
+}
