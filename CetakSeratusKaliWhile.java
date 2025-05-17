c1ef53b2e8181dcb38878c0f37d29b10cef0bc1b
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-17 12:53:04 +0700

    Create CetakSeratusKaliWhile.java

---
+++ CetakSeratusKaliWhile.java
@@ -0,0 +1,16 @@
+package com.mycompany.cetakseratuskaliwhile;
+
+/**
+ * Demonstrasi While Loop
+ * @author luqmanaru
+ */
+public class CetakSeratusKaliWhile {
+    public static void main(String[] args) {
+        int count = 0;
+        
+        while (count < 100) {
+            System.out.println("Hanif");
+            count++;
+        }
+    }
+}
